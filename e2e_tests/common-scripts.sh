3b7afc8fbd9458e93a53b157928cf3285b252133
Himanshu Rai <hrai@newrelic.com>
2025-01-13 11:18:14 +0530

    code refactor and added more testcases

---
+++ e2e_tests/common-scripts.sh
@@ -0,0 +1,209 @@
+#!/bin/bash
+
+source config-file.cfg
+
+log() {
+  echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
+}
+
+deploy_firehose_stack() {
+  local template_file=$1
+  local stack_name=$2
+  local license_key=$3
+  local new_relic_region=$4
+  local new_relic_account_id=$5
+  local store_secret_in_secret_manager=$6
+  local log_group_config=$7
+  local common_attributes=$8
+
+  log "Deploying Firehose stack: $stack_name"
+  sam deploy \
+    --template-file "$template_file" \
+    --stack-name "$stack_name" \
+    --parameter-overrides \
+      LicenseKey="$license_key" \
+      NewRelicRegion="$new_relic_region" \
+      NewRelicAccountId="$new_relic_account_id" \
+      StoreNRLicenseKeyInSecretManager="$store_secret_in_secret_manager" \
+      LogGroupConfig="$log_group_config" \
+      CommonAttributes="$common_attributes" \
+    --capabilities CAPABILITY_NAMED_IAM
+}
+
+validate_stack_deployment_status() {
+  local stack_name=$1
+
+  log "Validating stack deployment status for the stack : $stack_name"
+  local stack_status=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].StackStatus" --output text)
+  if [[ "$stack_status" == "ROLLBACK_COMPLETE" || "$stack_status" == "ROLLBACK_FAILED" || "$stack_status" == "CREATE_FAILED"  || "$stack_status" == "UPDATE_FAILED" ]]; then
+    log "Stack $stack_name failed to be created and rolled back."
+    local failure_reason=$(aws cloudformation describe-stack-events --stack-name "$stack_name" --query "StackEvents[?ResourceStatus==\`$stack_status\`].ResourceStatusReason" --output text)
+    exit_with_error "Stack $stack_name failed to be created. Failure reason: $failure_reason"
+  else
+    log "Stack $stack_name was created successfully."
+  fi
+}
+
+delete_stack() {
+  stack_name=$1
+
+  log "Initiating deletion of stack: $stack_name"
+  aws cloudformation delete-stack --stack-name "$stack_name"
+
+  local stack_status=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query 'Stacks[0].StackStatus' --output text)
+
+  # delete stack with exponential back off retires with max cap of 5 minutes
+  max_sleep_time=300  
+  sleep_time=30
+  while [[ $stack_status == "DELETE_IN_PROGRESS" ]]; do
+    log "Stack $stack_name is still being deleted..."
+    sleep $sleep_time
+    if (( sleep_time < max_sleep_time )); then
+      sleep_time=$(( sleep_time * 2 ))
+    fi
+    stack_status=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query 'Stacks[0].StackStatus' --output text 2>/dev/null || true)
+  done
+
+  if [ -z "$stack_status" ]; then
+    log "Stack $stack_name has been successfully deleted."
+  elif [ "$stack_status" == "DELETE_FAILED" ]; then
+    log "Failed to delete stack $stack_name."
+  else
+    log "Unexpected stack status: $stack_status."
+  fi
+}
+
+validate_stack_resources() {
+  local stack_name=$1
+  local validate_only_firehose_stack=$2
+  local log_group_name=$3
+  local log_group_filter=$4
+
+
+  log "Validating stack resources for stack: $stack_name"
+  firehose_stream_physical_id=$(aws cloudformation describe-stack-resources \
+                  --stack-name "$stack_name" \
+                  --logical-resource-id "$FIREHOSE_STREAM_LOGICAL_ID" \
+                  --query "StackResources[0].PhysicalResourceId" \
+                  --output text
+  )
+
+  # Get the ARN of the Firehose delivery stream using the physical ID
+  firehose_stream_arn=$(aws firehose describe-delivery-stream \
+                  --delivery-stream-name "$firehose_stream_physical_id" \
+                  --query "DeliveryStreamDescription.DeliveryStreamARN" \
+                  --output text
+  )
+
+  # Check firehose_stream_arn is not null before checking subscriptions
+  if [ -z "$firehose_stream_arn" ] || [ "$firehose_stream_arn" == "None" ]; then
+    exit_with_error "Failed to retrieve Firehose delivery stream ARN for physical ID: $firehose_stream_physical_id"
+  fi
+
+  # Skip log group subscription validation if validate_only_firehose_stack is true or log_group_filter is empty
+  if [ "$validate_only_firehose_stack" == "true" ] || [ -z "$log_group_filter" ]; then
+    log "Validated Firehose Stream, skipping log group subscription validation."
+    return
+  fi
+
+  subscriptions=$(aws logs describe-subscription-filters --log-group-name "$log_group_name" --query 'subscriptionFilters[*].[destinationArn, filterPattern]' --output text)
+
+  # Check if the Firehose delivery stream is subscribed to the log group with the specified filter pattern
+  if echo "$subscriptions" | grep -q "$firehose_stream_arn" && echo "$subscriptions" | grep -q "$log_group_filter"; then
+    log "Firehose Delivery Stream $firehose_stream_arn is subscribed to log group: $log_group_name with filter: $log_group_filter"
+  else
+    exit_with_error "Firehose Delivery Stream $firehose_stream_arn is not subscribed to log group: $log_group_name"
+  fi
+
+}
+
+
+exit_with_error() {
+  echo "Error: $1"
+  exit 1
+}
+
+create_log_event() {
+  local log_group_name=$1
+  local log_stream_name=$2
+  local log_message=$3
+
+  log "Creating log event in CloudWatch Log Group: $log_group_name"
+  local log_stream_exists=$(aws logs describe-log-streams --log-group-name "$log_group_name" --log-stream-name-prefix "$log_stream_name" --query "logStreams[?logStreamName=='$log_stream_name'] | length(@)" --output text)
+
+  if [ "$log_stream_exists" -eq 0 ]; then
+    log "Creating log stream: $log_stream_name"
+    aws logs create-log-stream --log-group-name "$log_group_name" --log-stream-name "$log_stream_name"
+  fi
+
+  # Get the current timestamp in milliseconds
+  timestamp=$(($(date +%s) * 1000 + $(date +%N) / 1000000))
+
+  # Put log event
+  aws logs put-log-events \
+    --log-group-name "$log_group_name" \
+    --log-stream-name "$log_stream_name" \
+    --log-events timestamp=$timestamp,message="$log_message"
+
+  log "Log event created successfully."
+
+}
+
+validate_logs_in_new_relic() {
+  local user_key=$1
+  local account_id=$2
+  local file_name=$3
+  local should_log_exist=$4
+
+  local nrql_query="SELECT * FROM Log WHERE message LIKE '%$file_name%' SINCE 10 minutes ago"
+  local query='{"query":"query($id: Int!, $nrql: Nrql!) { actor { account(id: $id) { nrql(query: $nrql) { results } } } }","variables":{"id":'$account_id',"nrql":"'$nrql_query'"}}'
+
+  local log_message_exists=false
+  for i in {1..10}; do
+    response=$(curl -s -X POST \
+      -H "Content-Type: application/json" \
+      -H "API-Key: $user_key" \
+      -d "$query" \
+      https://api.newrelic.com/graphql)
+
+    if echo "$response" | grep -q "$file_name"; then
+      log "Log event successfully found in New Relic."
+      log_message_exists=true
+
+      # Validate common attributes
+      for attribute in $(echo "$common_attributes" | jq -c '.[]'); do
+        attribute_name=$(echo "$attribute" | jq -r '.AttributeName')
+        attribute_value=$(echo "$attribute" | jq -r '.AttributeValue')
+        if ! echo "$response" | grep -q "\"$attribute_name\":\"$attribute_value\""; then
+          exit_with_error "Common attribute $attribute_name with value $attribute_value not found in New Relic logs."
+        fi
+      done
+      log "Common attributes validated successfully."
+
+      # Validate default attributes and their values
+      default_attributes=("instrumentation.provider=aws" "instrumentation.name=firehose" "instrumentation.version=1.0.0")
+      
+      for attribute in "${default_attributes[@]}"; do
+        key="${attribute%%=*}"
+        expected_value="${attribute##*=}"
+        if ! echo "$response" | grep -q "\"$key\":\"$expected_value\""; then
+          exit_with_error "Default attribute $key with value $expected_value not found in New Relic logs."
+        fi
+      done
+      log "Entity synthesis parameter validated successfully."
+
+      break
+    else
+      log "Log event not found in New Relic. Retrying in 30 seconds... ($i/10)"
+      sleep 30
+    fi
+  done
+
+  if [ "$should_log_exist" == "true" ] && [ "$log_message_exists" == "false" ]; then
+    exit_with_error "Log event not found in New Relic after 10 retries."
+  elif [ "$should_log_exist" == "false" ] && [ "$log_message_exists" == "true" ]; then
+    exit_with_error "Log event should not exist in New Relic, but it was found."
+  fi
+
+}
+
--- e2e_tests/firehose_e2e_tests.sh
+++ e2e_tests/firehose_e2e_tests.sh
@@ -1,163 +1,125 @@
 #!/bin/bash
 
 source config-file.cfg
+source common-scripts.sh
 
-deploy_firehose_stack() {
-  template_file=$1
-  stack_name=$2
-  license_key=$3
-  new_relic_region=$4
-  new_relic_account_id=$5
-  store_secret_in_secret_manager=$6
-  log_group_config=$7
-  common_attributes=$8
+# Test Case 1: 
+# Create a Firehose stack without any subscription filter pattern
+# Create a unique log message in CloudWatch Logs
+# Validate the logs in New Relic
+test_logs_without_filter_pattern() {
+  local template_file=$1
 
-  sam deploy \
-    --template-file "$template_file" \
-    --stack-name "$stack_name" \
-    --parameter-overrides \
-      LicenseKey="$license_key" \
-      NewRelicRegion="$new_relic_region" \
-      NewRelicAccountId="$new_relic_account_id" \
-      StoreNRLicenseKeyInSecretManager="$store_secret_in_secret_manager" \
-      LogGroupConfig="$log_group_config" \
-      CommonAttributes="$common_attributes" \
-    --capabilities CAPABILITY_NAMED_IAM
+cat <<EOF > parameter.json
+'[{"LogGroupName":"$LOG_GROUP_NAME_1"}]'
+EOF
+LOG_GROUP_NAME_JSON=$(<parameter.json)
 
+cat <<EOF > common_attribute.json
+'[{"AttributeName":"$COMMON_ATTRIBUTE_KEY","AttributeValue":"$COMMON_ATTRIBUTE_VALUE"}]'
+EOF
+COMMON_ATTRIBUTES=$(<common_attribute.json)
+
+  # Deploy the Firehose stack
+  deploy_firehose_stack "$template_file" "$FIREHOSE_STACK_NAME_1" "$NEW_RELIC_LICENSE_KEY" "$NEW_RELIC_REGION" "$NEW_RELIC_ACCOUNT_ID" "true" "$LOG_GROUP_NAME_JSON" "$COMMON_ATTRIBUTES"
+  
+  # Validate the status of the Firehose stack
+  validate_stack_deployment_status "$FIREHOSE_STACK_NAME_1"
+
+  # Validate the stack resources
+
+  validate_stack_resources "$FIREHOSE_STACK_NAME_1" "false" "$LOG_GROUP_NAME_1" ""
+  # Generate a UUID and create a dynamic log message
+  UUID=$(uuidgen)
+  LOG_MESSAGE="RequestId: $UUID hello world"
+
+  # Create a log event in CloudWatch Logs
+  create_log_event "$LOG_GROUP_NAME_1" "$LOG_STREAM_NAME" "$LOG_MESSAGE"
+
+  # Validate logs in New Relic
+  validate_logs_in_new_relic "$NEW_RELIC_USER_KEY" "$NEW_RELIC_ACCOUNT_ID" "$LOG_MESSAGE" "true"
+
+  # Delete the Firehose stack
+  delete_stack "$FIREHOSE_STACK_NAME_1"
 }
 
-validate_stack_deployment_status() {
-  stack_name=$1
+# Test Case 2: 
+# Create a Firehose stack with a subscription filter pattern
+# Create a unique log message with subscription filter pattern present in the log message
+# Validate the logs in New Relic
+# Create a unique log message without the subscription filter pattern
+# Validate that the log message should not exist in New Relic
 
-  stack_status=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].StackStatus" --output text)
-  if [[ "$stack_status" == "ROLLBACK_COMPLETE" || "$stack_status" == "ROLLBACK_FAILED" || "$stack_status" == "CREATE_FAILED"  || "$stack_status" == "UPDATE_FAILED" ]]; then
-    echo "Stack $stack_name failed to be created and rolled back."
-    failure_reason=$(aws cloudformation describe-stack-events --stack-name "$stack_name" --query "StackEvents[?ResourceStatus==\`$stack_status\`].ResourceStatusReason" --output text)
-    exit_with_error "Stack $stack_name failed to be created. Failure reason: $failure_reason"
-  else
-    echo "Stack $stack_name was created successfully."
-  fi
+test_logs_with_filter_pattern() {
+  local template_file=$1
+
+cat <<EOF > parameter.json
+'[{"LogGroupName":"$LOG_GROUP_NAME_2","FilterPattern":"$LOG_GROUP_FILTER_PATTERN"}]'
+EOF
+LOG_GROUP_NAME_JSON=$(<parameter.json)
+
+cat <<EOF > common_attribute.json
+'[{"AttributeName":"$COMMON_ATTRIBUTE_KEY","AttributeValue":"$COMMON_ATTRIBUTE_VALUE"}]'
+EOF
+COMMON_ATTRIBUTES=$(<common_attribute.json)
+
+  # Deploy the Firehose stack
+  deploy_firehose_stack "$template_file" "$FIREHOSE_STACK_NAME_2" "$NEW_RELIC_LICENSE_KEY" "$NEW_RELIC_REGION" "$NEW_RELIC_ACCOUNT_ID" "true" "$LOG_GROUP_NAME_JSON" "$COMMON_ATTRIBUTES"
+  
+  # Validate the status of the Firehose stack
+  validate_stack_deployment_status "$FIREHOSE_STACK_NAME_2"
+
+  # Validate the stack resources
+  validate_stack_resources "$FIREHOSE_STACK_NAME_2" "false" "$LOG_GROUP_NAME_2" "$LOG_GROUP_FILTER_PATTERN"
+
+  # Generate a UUID and create a dynamic log message with the filter pattern
+  UUID=$(uuidgen)
+  LOG_MESSAGE="RequestId: $UUID hello world $LOG_GROUP_FILTER_PATTERN"
+
+  # Create a log event in CloudWatch Logs
+  create_log_event "$LOG_GROUP_NAME_2" "$LOG_STREAM_NAME" "$LOG_MESSAGE"
+
+  # Validate logs in New Relic (should exist)
+  validate_logs_in_new_relic "$NEW_RELIC_USER_KEY" "$NEW_RELIC_ACCOUNT_ID" "$LOG_MESSAGE" "true"
+
+   # Generate a UUID and create a dynamic log message without the filter pattern
+  UUID=$(uuidgen)
+  LOG_MESSAGE="RequestId: $UUID hello world"
+
+  # Create a log event in CloudWatch Logs
+  create_log_event "$LOG_GROUP_NAME_2" "$LOG_STREAM_NAME" "$LOG_MESSAGE"
+
+  # Validate logs in New Relic (should not exist)
+  validate_logs_in_new_relic "$NEW_RELIC_USER_KEY" "$NEW_RELIC_ACCOUNT_ID" "$LOG_MESSAGE" "false"
+
+  # Delete the Firehose stack
+  delete_stack "$FIREHOSE_STACK_NAME_2"
 }
 
-delete_stack() {
-  stack_name=$1
+# Test Case 3: 
+# Creating Firehose stack with Invalid Log Group Name 
+# Validate that the Firehose stack is created successfully with Firehose delivery stream
+test_logs_with_invalid_log_group() {
+  local template_file=$1
 
-  aws cloudformation delete-stack --stack-name "$stack_name"
+cat <<EOF > parameter.json
+'[{"LogGroupName":"$INVALID_LOG_GROUP_NAME"}]'
+EOF
+LOG_GROUP_NAME_JSON=$(<parameter.json)
 
-  echo "Initiated deletion of stack: $stack_name"
+  # Deploy the Firehose stack
+  deploy_firehose_stack "$template_file" "$FIREHOSE_STACK_NAME_3" "$NEW_RELIC_LICENSE_KEY" "$NEW_RELIC_REGION" "$NEW_RELIC_ACCOUNT_ID" "true" "$LOG_GROUP_NAME_JSON" "''"
+  
+  # Validate the status of the Firehose stack
+  validate_stack_deployment_status "$FIREHOSE_STACK_NAME_3"
 
-  stack_status=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query 'Stacks[0].StackStatus' --output text)
+  # Validate the stack resources
+  validate_stack_resources "$FIREHOSE_STACK_NAME_3" "true" "''" "''"
 
-  while [[ $stack_status == "DELETE_IN_PROGRESS" ]]; do
-    echo "Stack $stack_name is still being deleted..."
-    sleep 60
-    stack_status=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query 'Stacks[0].StackStatus' --output text 2>/dev/null || true)
-  done
-
-  if [ -z "$stack_status" ]; then
-    echo "Stack $stack_name has been successfully deleted."
-  elif [ "$stack_status" == "DELETE_FAILED" ]; then
-    echo "Failed to delete stack $stack_name."
-  else
-    echo "Unexpected stack status: $stack_status."
-  fi
+  # Delete the Firehose stack
+  delete_stack "$FIREHOSE_STACK_NAME_3"
 }
-
-validate_stack_resources() {
-  stack_name=$1
-  log_group_name=$2
-  log_group_filter=$3
-
-  firehose_stream_physical_id=$(aws cloudformation describe-stack-resources \
-                  --stack-name "$stack_name" \
-                  --logical-resource-id "$FIREHOSE_STREAM_LOGICAL_ID" \
-                  --query "StackResources[0].PhysicalResourceId" \
-                  --output text
-  )
-
-  # Get the ARN of the Firehose delivery stream using the physical ID
-  firehose_stream_arn=$(aws firehose describe-delivery-stream \
-                  --delivery-stream-name "$firehose_stream_physical_id" \
-                  --query "DeliveryStreamDescription.DeliveryStreamARN" \
-                  --output text
-  )
-
-  subscriptions=$(aws logs describe-subscription-filters --log-group-name "$log_group_name" --query 'subscriptionFilters[*].[destinationArn, filterPattern]' --output text)
-
-  # Check firehose_stream_arn is not null before checking subscriptions
-  if [ -z "$firehose_stream_arn" ] || [ "$firehose_stream_arn" == "None" ]; then
-    exit_with_error "Failed to retrieve Firehose delivery stream ARN for physical ID: $firehose_stream_physical_id"
-  fi
-
-  # Check if the Firehose delivery stream is subscribed to the log group with the specified filter pattern
-  if echo "$subscriptions" | grep -q "$firehose_stream_arn" && echo "$subscriptions" | grep -q "$log_group_filter"; then
-    echo "Firehose Delivery Stream $firehose_stream_arn is subscribed to log group: $log_group_name with filter: $log_group_filter"
-  else
-    exit_with_error "Firehose Delivery Stream $firehose_stream_arn is not subscribed to log group: $log_group_name"
-  fi
-
-}
-
-exit_with_error() {
-  echo "Error: $1"
-  exit 1
-}
-
-create_log_event() {
-  echo "Creating log event in CloudWatch Log Group"
-  log_group_name=$1
-  log_stream_name=$2
-  log_message=$3
-
-  # Check if the log stream exists
-  log_stream_exists=$(aws logs describe-log-streams --log-group-name "$log_group_name" --log-stream-name-prefix "$log_stream_name" --query "logStreams[?logStreamName=='$log_stream_name'] | length(@)" --output text)
-
-  # Create a log stream
-  if [ "$log_stream_exists" -eq 0 ]; then
-    # Create a log stream if it does not exist
-    aws logs create-log-stream --log-group-name "$log_group_name" --log-stream-name "$log_stream_name"
-  fi
-
-  # Get the current timestamp in milliseconds
-  timestamp=$(($(date +%s) * 1000 + $(date +%N) / 1000000))
-
-  # Put log event
-  aws logs put-log-events \
-    --log-group-name "$log_group_name" \
-    --log-stream-name "$log_stream_name" \
-    --log-events timestamp=$timestamp,message="$log_message"
-
-  echo "Log event created successfully."
-
-}
-
-validate_logs_in_new_relic() {
-  user_key=$1
-  account_id=$2
-  file_name=$3
-
-  nrql_query="SELECT * FROM Log WHERE message LIKE '%$file_name%' SINCE 10 minutes ago"
-  query='{"query":"query($id: Int!, $nrql: Nrql!) { actor { account(id: $id) { nrql(query: $nrql) { results } } } }","variables":{"id":'$account_id',"nrql":"'$nrql_query'"}}'
-
-  for i in {1..10}; do
-    response=$(curl -s -X POST \
-      -H "Content-Type: application/json" \
-      -H "API-Key: $user_key" \
-      -d "$query" \
-      https://api.newrelic.com/graphql)
-
-    if echo "$response" | grep -q "$file_name"; then
-      echo "Log event successfully found in New Relic."
-      return 0
-    else
-      echo "Log event not found in New Relic. Retrying in 30 seconds... ($i/10)"
-      sleep 30
-    fi
-  done
-
-  exit_with_error "Log event not found in New Relic after 10 retries."
-}
-
+  
 
 
 BASE_NAME=$(basename "$TEMPLATE_FILE_NAME" .yaml)
@@ -167,34 +129,14 @@ BUILD_DIR="$BUILD_DIR_BASE/$BASE_NAME"
 sam build --template-file "../$TEMPLATE_FILE_NAME" --build-dir "$BUILD_DIR"
 sam package --s3-bucket "$S3_BUCKET" --template-file "$BUILD_DIR/template.yaml" --output-template-file "$BUILD_DIR/$TEMPLATE_FILE_NAME"
 
+# Run Test Case 1: Logs without filter pattern
+test_logs_without_filter_pattern  "$BUILD_DIR/$TEMPLATE_FILE_NAME" &
 
-cat <<EOF > parameter.json
-'[{"LogGroupName":"$LOG_GROUP_NAME","FilterPattern":"$LOG_GROUP_FILTER_PATTERN"}]'
-EOF
-LOG_GROUP_NAMES=$(<parameter.json)
+# Run Test Case 2: Logs with filter pattern
+test_logs_with_filter_pattern "$BUILD_DIR/$TEMPLATE_FILE_NAME" &
 
-# Generate a random string to append to the default stack name
-RANDOM_STRING=$(openssl rand -hex 4)
-FIREHOSE_STACK_NAME="${DEFAULT_STACK_NAME}-${RANDOM_STRING}"
+# Run Test Case 3: Create stack with invalid log group
+test_logs_with_invalid_log_group "$BUILD_DIR/$TEMPLATE_FILE_NAME" &
 
-# Deploy the Firehose stack
-deploy_firehose_stack "$BUILD_DIR/$TEMPLATE_FILE_NAME" "$FIREHOSE_STACK_NAME" "$NEW_RELIC_LICENSE_KEY" "$NEW_RELIC_REGION" "$NEW_RELIC_ACCOUNT_ID" "true" "$LOG_GROUP_NAMES" "''"
-
-# Validate the status of the Firehose stack
-validate_stack_deployment_status "$FIREHOSE_STACK_NAME"
-
-# Validate the resources created by the Firehose stack
-validate_stack_resources "$FIREHOSE_STACK_NAME" "$LOG_GROUP_NAME" "$LOG_GROUP_FILTER_PATTERN"
-
-# Generate a UUID and create a dynamic log message
-UUID=$(uuidgen)
-LOG_MESSAGE="RequestId: $UUID hello world $LOG_GROUP_FILTER_PATTERN"
-
-# Create a log event in CloudWatch Logs
-create_log_event "$LOG_GROUP_NAME" "$LOG_STREAM_NAME" "$LOG_MESSAGE"
-
-# Validate logs in New Relic
-validate_logs_in_new_relic "$NEW_RELIC_USER_KEY" "$NEW_RELIC_ACCOUNT_ID" "$LOG_MESSAGE"
-
-# Delete the Firehose stack
-delete_stack "$FIREHOSE_STACK_NAME"
+# Wait for all background jobs to complete
+wait

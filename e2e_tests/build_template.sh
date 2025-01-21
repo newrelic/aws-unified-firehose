21e9e29a88dce8b62b23659312d0fb40aa3602c1
Himanshu Rai <hrai@newrelic.com>
2025-01-21 12:38:23 +0530

    separating build process

---
+++ e2e_tests/build_template.sh
@@ -0,0 +1,7 @@
+#Building and deploying the changes
+BASE_NAME=$(basename "$TEMPLATE_FILE_NAME" .yaml)
+BUILD_DIR="$BUILD_DIR_BASE/$BASE_NAME"
+
+# Build and package the SAM template
+sam build --template-file "../$TEMPLATE_FILE_NAME" --build-dir "$BUILD_DIR"
+sam package --s3-bucket "$S3_BUCKET" --template-file "$BUILD_DIR/template.yaml" --output-template-file "$BUILD_DIR/$TEMPLATE_FILE_NAME"
--- e2e_tests/firehose_e2e_tests.sh
+++ e2e_tests/firehose_e2e_tests.sh
@@ -153,17 +153,8 @@ COMMON_ATTRIBUTES=$(<common_attribute.json)
   # Delete the Firehose stack
   delete_stack "$FIREHOSE_STACK_NAME_4"
 }
-  
 
-
-BASE_NAME=$(basename "$TEMPLATE_FILE_NAME" .yaml)
-BUILD_DIR="$BUILD_DIR_BASE/$BASE_NAME"
-
-# Build and package the SAM template
-sam build --template-file "../$TEMPLATE_FILE_NAME" --build-dir "$BUILD_DIR"
-sam package --s3-bucket "$S3_BUCKET" --template-file "$BUILD_DIR/template.yaml" --output-template-file "$BUILD_DIR/$TEMPLATE_FILE_NAME"
-
-Run the test cases
+#Run the test cases
 case $1 in
   test-without-filter)
     test_logs_without_filter_pattern 

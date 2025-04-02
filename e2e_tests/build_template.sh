63e5a01d79cd89980b1e01a4c5351d34fbcfa2ef
Himanshu Rai <hrai@newrelic.com>
2025-04-02 15:29:53 +0530

    test_hrai_11

--- e2e_tests/build_template.sh
+++ e2e_tests/build_template.sh
@@ -3,15 +3,17 @@ BASE_NAME=$(basename "$TEMPLATE_FILE_NAME" .yaml)
 BUILD_DIR="$BASE_NAME"
 
 # Build and package the SAM template
+pwd
+cd ..
 cat $TEMPLATE_FILE_NAME
 pwd
 echo "Building and packaging the SAM template"
 BASE_NAME
 BUILD_DIR
 echo "Building and packaging the SAM template"
-version=$(grep 'instrumentation_version' ../../version.yaml | cut -d '"' -f2)
+version=$(grep 'instrumentation_version' version.yaml | cut -d '"' -f2)
 echo "Building and packaging the SAM template with version: $version"
 sed -i "s/\"instrumentation.version\", \"AttributeValue\": \"[^\"]*\"/\"instrumentation.version\", \"AttributeValue\": \"$version\"/" $TEMPLATE_FILE_NAME
 cat $TEMPLATE_FILE_NAME
-sam build --template-file "../$TEMPLATE_FILE_NAME" --build-dir "$BUILD_DIR"
+sam build --template-file "$TEMPLATE_FILE_NAME" --build-dir "$BUILD_DIR"
 sam package --s3-bucket "$S3_BUCKET" --template-file "$BUILD_DIR/template.yaml" --output-template-file "$BUILD_DIR/$TEMPLATE_FILE_NAME"

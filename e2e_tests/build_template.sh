fe2b4203e4b3ea587446b5dfdc4ba35a15a533d1
hrai-nr <hrai@newrelic.com>
2025-04-09 13:52:47 +0530

    Adding test for version changes in E2E flow (#24)
    Added version changes for E2E test and hardcoded the sam cli version to 1.134.0

--- e2e_tests/build_template.sh
+++ e2e_tests/build_template.sh
@@ -3,5 +3,9 @@ BASE_NAME=$(basename "$TEMPLATE_FILE_NAME" .yaml)
 BUILD_DIR="$BASE_NAME"
 
 # Build and package the SAM template
+echo "Finding the version in version.yaml"
+version=$(grep 'instrumentation_version' ../version.yaml | cut -d '"' -f2)
+echo "Version found is: $version"
+sed -i "s/\"instrumentation.version\", \"AttributeValue\": \"[^\"]*\"/\"instrumentation.version\", \"AttributeValue\": \"$version\"/" ../$TEMPLATE_FILE_NAME
 sam build --template-file "../$TEMPLATE_FILE_NAME" --build-dir "$BUILD_DIR"
 sam package --s3-bucket "$S3_BUCKET" --template-file "$BUILD_DIR/template.yaml" --output-template-file "$BUILD_DIR/$TEMPLATE_FILE_NAME"
